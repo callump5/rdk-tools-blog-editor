1866f226bcc2c77739c40cb52df7f79ad6aa77a9
callump5 <clpullinger@gmail.com>
2026-04-15 22:29:20 +0100

    changed to livewire component

--- src/BlogEditorServiceProvider.php
+++ src/BlogEditorServiceProvider.php
@@ -11,12 +11,14 @@ class BlogEditorServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // OR register with class path
         Livewire::addNamespace(
             namespace: 'blog-editor',
             classNamespace: 'RdkTools\\BlogEditor\\Livewire',
+
             classPath: __DIR__ . '/Livewire',
             classViewPath: __DIR__ . '/../resources/views/livewire',
         );
+
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'blog-editor');
     }
 }
---
+++ src/Livewire/BlogEditor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RdkTools\BlogEditor\Livewire;
+
+use Livewire\Component;
+
+class BlogEditor extends Component
+{
+    public function render()
+    {
+        return view('blog-editor::livewire.blog-editor');
+    }
+}
