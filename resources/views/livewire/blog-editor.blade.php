60fd93edf46e00062204924dcb298f4aa1d17431
callump5 <clpullinger@gmail.com>
2026-05-17 11:20:46 +0100

    pushing height

--- resources/views/livewire/blog-editor.blade.php
+++ resources/views/livewire/blog-editor.blade.php
@@ -78,6 +78,7 @@
                         if (i.classList.contains('active')) return;
 
                         const iframe = i.querySelector('iframe');
+                        iframe.style.height = '700px'
 
                         if (!iframe) return;
                         const doc = iframe.contentDocument || iframe.contentWindow.document;
@@ -94,7 +95,7 @@
                         doc.designMode = 'on';
                         doc.body.style.fontFamily = 'Arial';
                         doc.body.style.padding = '10px 0 ';
-                        doc.body.style.height = '1000px';
+                        doc.body.style.height = '700px';
 
                         // Store doc by key
                         editors[elementKey] = doc;
